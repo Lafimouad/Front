19227cbf978f12ee8b1749d25d5cea11b3c4dd08
mayssaabdellaoui123 <58725994+mayssaabdellaoui123@users.noreply.github.com>
2021-05-08 01:06:32 +0200

    authority shelf

--- src/app/listshelfs/listshelfs.component.ts
+++ src/app/listshelfs/listshelfs.component.ts
@@ -4,6 +4,7 @@ import { ShelfService } from 'src/app/shelf.service';
 import { Observable,Subject } from "rxjs";  
 import {FormControl,FormGroup,NgForm,Validators} from '@angular/forms';  
 import { HttpErrorResponse } from '@angular/common/http';
+import { TokenStorgeService } from '../token-storage.service';
   
 
 @Component({
@@ -13,12 +14,50 @@ import { HttpErrorResponse } from '@angular/common/http';
 })
 export class ListshelfsComponent implements OnInit {
 
+  private roles: string[];
+  public authority: string;
+  public authoritymanager : boolean = false ;
+  public authorityadmin : boolean = false ;
+  public authorityclient : boolean = false ;
+  public authoritydeliverer : boolean = false ;
+  info : any ; 
+  //////////////////////////////////////////////////////////////
+
   public shelfs: Shelf[];
   public editShelf: Shelf;
   public deleteshelf: Shelf;
   
-  constructor(private shelfservice: ShelfService){}
+  constructor(private shelfservice: ShelfService,private tokenStorage: TokenStorgeService , private token:TokenStorgeService ){}
   ngOnInit() {
+    this.info = {
+      token: this.token.getToken(),
+      username: this.token.getUsername(),
+      authorities: this.token.getAuthorities()};
+
+      if (this.tokenStorage.getToken()) {
+        this.roles = this.tokenStorage.getAuthorities();
+        this.roles.every(role => {
+          if (role === 'ROLE_ADMIN') {
+            this.authority = 'admin';
+            this.authorityadmin = true;
+            return false;
+          } else if (role === 'ROLE_CLIENT') {
+            this.authority = 'client';
+            this.authorityclient = true;
+            return false;
+          }else if (role === 'ROLE_DELIVERER') {
+            this.authority = 'deliverer';
+            this.authoritydeliverer = true;
+            return false;
+          }else if (role === 'ROLE_MANAGER') {
+            this.authority = 'manager';
+            this.authoritymanager = true;
+            return false;
+          }
+          this.authority = 'user';
+          return true;
+        }); }
+
     this.getShelves();
   }
 
