e2d95a857b25713e600b3308c1e92928b6ecdac6
mayssaabdellaoui123 <58725994+mayssaabdellaoui123@users.noreply.github.com>
2021-05-07 23:41:27 +0200

    crud shelf version 2

--- src/app/listshelfs/listshelfs.component.ts
+++ src/app/listshelfs/listshelfs.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Shelf } from '../shelfstock/Shelf';
 import { ShelfService } from 'src/app/shelf.service';
 import { Observable,Subject } from "rxjs";  
-import {FormControl,FormGroup,Validators} from '@angular/forms';  
+import {FormControl,FormGroup,NgForm,Validators} from '@angular/forms';  
 import { HttpErrorResponse } from '@angular/common/http';
   
 
@@ -14,7 +14,8 @@ import { HttpErrorResponse } from '@angular/common/http';
 export class ListshelfsComponent implements OnInit {
 
   public shelfs: Shelf[];
-
+  public editShelf: Shelf;
+  public deleteshelf: Shelf;
   
   constructor(private shelfservice: ShelfService){}
   ngOnInit() {
@@ -35,6 +36,49 @@ export class ListshelfsComponent implements OnInit {
        
     
   }
+
+  public onAddShelves(addForm: NgForm): void{
+    document.getElementById('add-employee-form').click();
+    this.shelfservice.addShelfs(addForm.value).subscribe(
+      (response: Shelf) => {
+        console.log(response);
+        this.getShelves();
+        addForm.reset();
+      },
+      (error: HttpErrorResponse) => {
+        alert(error.message);
+        addForm.reset();
+
+      }
+    )  }
+
+
+    public onDeleteShelves(shelfId: number): void{
+      this.shelfservice.deleteShelfs(shelfId).subscribe(
+        (response: void) => {
+          console.log(response);
+          this.getShelves();
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+  
+        }
+      )  }
+
+
+    public onUpdateShelves(shelf: Shelf): void{
+      this.shelfservice.updateShelfs(shelf).subscribe(
+        (response: Shelf) => {
+          console.log(response);
+          this.getShelves();
+        },
+        (error: HttpErrorResponse) => {
+          alert(error.message);
+        }
+      )  }
+  
+
+
   public onOpenModal(shelf: Shelf,mode: string): void{
       const container = document.getElementById('main-container');
       const button = document.createElement('button')
@@ -45,9 +89,11 @@ export class ListshelfsComponent implements OnInit {
         button.setAttribute('data-target','#addShelfModal')
       }  
       if (mode == 'edit') {
+        this.editShelf = shelf;
         button.setAttribute('data-target','#updateShelfModal')
       }  
       if (mode == 'delete') {
+        this.deleteshelf = shelf;
         button.setAttribute('data-target','#deleteShelfModal')
       }  
       container.appendChild(button);
